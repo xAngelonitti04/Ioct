096262e8a5e34ad2995d0bac92b1bf91fc93e288
xAngelonitti04 <angelonitti45@gmail.com>
2026-05-20 19:02:48 +0200

    docker funziona

--- database.py
+++ database.py
@@ -1,10 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:@localhost:3306/ioct_cultural_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:root@db:3306/ioct_cultural_db"
+)
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
 
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
